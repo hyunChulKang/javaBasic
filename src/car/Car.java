42a52b597d883f8ad7dfd45351ecdb4848b7d599
HyunChulKang <godapple123123@gmail.com>
2019-12-13 21:57:16 +0900

    car

---
+++ src/car/Car.java
@@ -0,0 +1,36 @@
+package com.inja.test;
+
+public class Car {
+
+	//실린더 -> 피스톤 ->폭발 -> 컨넥팅로드(회전운동) -> 플라이 휠(회전) -> 기어작동(기어변속)->구동축 (회전)-> 톱니기어 -> 차량의 휠-> 차 굴러감
+		int sillinthe;
+		int piston;
+		int boom;
+		int connectingRoad;
+		int flingWhell;
+		int gear;
+		int tobniGear;
+		int Whell;
+
+}
+class Person{
+
+	void drive(){
+		
+	}
+	void gearChange(){
+		
+	}
+
+	void breakgo(){
+		
+	}
+	void axel(){
+		
+	}
+	void clutch(){
+		
+	}
+
+}
+
